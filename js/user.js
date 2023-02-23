6c20f51d71a022c4d819faa0b4dbc679e6bf52a2
kapsin2 <110449509+kapsin2@users.noreply.github.com>
2023-02-23 15:49:09 +0900

    #5[feat] 메인페이지 기능추가

--- js/user.js
+++ js/user.js
@@ -83,75 +83,38 @@ function logout() {
 
 
 function profileUpdate() {
-  localStorage.removeItem("imageUrl");
-  uploadImage().then(() => {
-    const auth = getToken();
-    let nickname = $('#formGroupExampleInput').val();
-    let introduction = $('#formGroupExampleInput2').val();
-    let image = localStorage.getItem("imageUrl");
+  const auth = getToken();
 
-    console.log(image);
-    var settings = {
-      "url": "http://localhost:8080/users/profiles",
-      "method": "PATCH",
-      "timeout": 0,
-      "headers": {
-        "Content-Type": "application/json"
-      },
-      "data": JSON.stringify({
-        "nickname": nickname,
-        "introduction": introduction,
-        "image" : image
-      }),
-      "beforeSend": function(xhr) {
-        xhr.setRequestHeader("Authorization", auth);
-      }
-    };
+  let image = $('#formGroupExampleInput3').val();
+  let nickname = $('#formGroupExampleInput').val();
+  let introduction = $('#formGroupExampleInput2').val();
 
-    $.ajax(settings).done(function (response) {
-      console.log(response);
-      if (response == "success"){
-        alert("프로필 수정 완료")
-        window.location.href = 'http://127.0.0.1:5501/MyProfile.html';
-      }
-    });
-  })
-}
 
-function uploadImage() {
-  return new Promise((resolve, reject) => {
-    const auth = getToken();
-
-    const fileInput = document.getElementById("formGroupExampleInput3");
-    const file = fileInput.files[0];
-    const formData = new FormData();
-    formData.append("file", file);
-
-    var settings = {
-      "url": "http://localhost:8080/upload",
-      "method": "POST",
-      "timeout": 0,
-      "headers": {},
-      "processData": false,
-      "mimeType": "multipart/form-data",
-      "contentType": false,
-      "data": formData,
-      "beforeSend": function(xhr) {
-        xhr.setRequestHeader("Authorization", auth);
-      }
-    };
-
-    $.ajax(settings).done(function (response) {
-      console.log(response);
-      localStorage.setItem("imageUrl", response); // 저장소에 이미지 URL 저장
-      resolve(); // 이미지 업로드 완료를 알림
-    }).fail(function (jqXHR, textStatus, errorThrown) {
-      reject(errorThrown); // 에러가 발생한 경우 Promise를 reject()로 반환
-    });
+  var settings = {
+    "url": "http://localhost:8080/users/profiles",
+    "method": "PATCH",
+    "timeout": 0,
+    "headers": {
+      "Content-Type": "application/json"
+    },
+    "data": JSON.stringify({
+      "nickname": nickname,
+      "introduction": introduction,
+      "image": image
+    }),
+    "beforeSend": function(xhr) {
+      xhr.setRequestHeader("Authorization", auth);
+    }
+  };
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    if (response == "success"){
+      alert("프로필 수정 완료")
+      window.location.href = 'http://127.0.0.1:5501/MyProfile.html';
+    }
   });
 }
-
-
 
 function getAllUsers() {
   sendAuthorizedRequest("http://localhost:8080/admin/users", "GET", function (response) {
